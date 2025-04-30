2aaaac18d68d14c18eed38f61d28f9d10acc2a39
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-30 21:54:52 +0600

    advanced calendar with custom styling explored

--- 03 - React-Big-Calendar/src/components/AdvancedCalendar.tsx
+++ 03 - React-Big-Calendar/src/components/AdvancedCalendar.tsx
@@ -6,16 +6,49 @@ const events = [
     start: moment('2025-04-30T10:00:00').toDate(),
     end: moment('2025-04-30T11:00:00').toDate(),
     title: 'MRI Registration',
+    data: {
+      type: "Reg",
+    }
   },
   {
     start: moment('2025-04-30T14:00:00').toDate(),
     end: moment('2025-04-30T15:30:00').toDate(),
     title: 'ENT Appointment',
+    data: {
+      type: 'App'
+    }
   },
 ];
 
-
+const components = {
+  event: (props) => {
+    const eventType = props?.event?.data?.type
+    switch (eventType) {
+      case "Reg": 
+        return (
+          <div
+            style={{
+              background: 'yellow',
+              color: 'black',
+              padding: '10px',
+              borderRadius: '4px',
+            }}
+          >
+            {props.title}
+          </div>
+        );
+      case "App": 
+        return <div style={{ background: 'lightgreen', color: 'black', padding: '10px', borderRadius: '4px' }}>{props.title}</div>
+        
+  
+    
+      default:
+        return null;
+    }
+    
+}
+}
 
 export default function AdvancedCalendar() {
-  return <MyCalendar events={events} />;
+  return <MyCalendar events={events} components={components}/>;
 }
