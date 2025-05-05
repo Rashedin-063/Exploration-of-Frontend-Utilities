f09f98858a7b309e1a53ec00b0e81b5926d0ed7b
Rashedin-063 <rashedinislam.06@gmail.com>
2025-05-05 11:11:33 +0600

    code updated

--- 04 - React-Dropzone/src/pages/contact.tsx
+++ 04 - React-Dropzone/src/pages/contact.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useState } from 'react';
-import { useDropzone} from 'react-dropzone';
+import { useState } from 'react';
 
 import Layout from '@/components/Layout';
 import Container from '@/components/Container';
@@ -8,22 +7,7 @@ import FormLabel from '@/components/FormLabel';
 import InputText from '@/components/InputText';
 import Button from '@/components/Button';
 
-
 function Contact() {
-  const onDrop = useCallback((acceptedFiles: Array<File>) => {
-    const file = new FileReader;
-
-    file.onload = function() {
-      setPreview(file.result);
-    }
-
-    file.readAsDataURL(acceptedFiles[0])
-  }, [])
-
-  const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop
-  });
-
   const [preview, setPreview] = useState<string | ArrayBuffer | null>(null);
 
   /**
@@ -33,70 +17,46 @@ function Contact() {
   async function handleOnSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
 
-    if ( typeof acceptedFiles[0] === 'undefined' ) return;
-
-    const formData = new FormData();
-
-    formData.append('file', acceptedFiles[0]);
-    formData.append('upload_preset', '<Your Upload Preset>');
-    formData.append('api_key', import.meta.env.VITE_CLOUDINARY_API_KEY);
-
-    const results = await fetch('https://api.cloudinary.com/v1_1/<Your Cloud Name>/image/upload', {
-      method: 'POST',
-      body: formData
-    }).then(r => r.json());
-
-    console.log('results', results);
+    // ...existing code...
   }
 
   return (
     <Layout>
-
       <Container>
-        <h1 className="text-6xl font-black text-center text-slate-900 mb-20">
+        <h1 className='text-6xl font-black text-center text-slate-900 mb-20'>
           Contact Us
         </h1>
-        
-        <form className="max-w-md border border-gray-200 rounded p-6 mx-auto" onSubmit={handleOnSubmit}>
-          <FormRow className="mb-5">
-            <FormLabel htmlFor="name">Name</FormLabel>
-            <InputText id="name" name="name" type="text" />
+
+        <form
+          className='max-w-md border border-gray-200 rounded p-6 mx-auto'
+          onSubmit={handleOnSubmit}
+        >
+          <FormRow className='mb-5'>
+            <FormLabel htmlFor='name'>Name</FormLabel>
+            <InputText id='name' name='name' type='text' />
           </FormRow>
 
-          <FormRow className="mb-5">
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <InputText id="email" name="email" type="email" />
-          </FormRow>
-          
-          <FormRow className="mb-5">
-            <FormLabel htmlFor="message">Message</FormLabel>
-            <InputText id="message" name="message" type="text" />
+          <FormRow className='mb-5'>
+            <FormLabel htmlFor='email'>Email</FormLabel>
+            <InputText id='email' name='email' type='email' />
           </FormRow>
 
-          <FormRow className="mb-5">
-            <FormLabel htmlFor="image">Image</FormLabel>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              {
-                isDragActive ?
-                  <p>Drop the files here ...</p> :
-                  <p>Drag 'n' drop some files here, or click to select files</p>
-              }
-            </div>
+          <FormRow className='mb-5'>
+            <FormLabel htmlFor='message'>Message</FormLabel>
+            <InputText id='message' name='message' type='text' />
           </FormRow>
 
           {preview && (
-            <p className="mb-5">
-              <img src={preview as string} alt="Upload preview" />
+            <p className='mb-5'>
+              <img src={preview as string} alt='Upload preview' />
             </p>
           )}
 
           <Button>Submit</Button>
         </form>
-
       </Container>
     </Layout>
-  )
+  );
 }
 
 export default Contact;
